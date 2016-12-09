721c8ff2094c36d4f93119d66b702073861fc6ee
Shannon Wu <shannwu@stanford.edu>
2016-12-08 16:29:34 -0800

    added new invite screen (notes: need to update the intents for the buttons)

---
+++ app/src/main/java/example/com/gracie/muse/inviteToExistingStrip.java
@@ -0,0 +1,13 @@
+package example.com.gracie.muse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inviteToExistingStrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_to_existing_strip);
+    }
+}
